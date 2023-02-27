2c0385b98bfb4609aa18b378baff2cf44ff3333d
Josh Keeler <keelerjr12@gmail.com>
2023-02-26 20:21:12 -0600

    added cpp file

--- main.cpp
+++ main.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
 #include <vector>
-#include "utility.h"
 #include "chp6.h"
 
 int main() {
 
-    std::vector<int> vec = { 5, 3, 2, 1, 4};
-    Heap heap(vec);
+    //Keeler::print(std::begin(heap.Data()), std::end(heap.Data()));
 
-    Keeler::print(std::begin(heap.Data()), std::end(heap.Data()));
-
-    heap.Sort();
-
-    Keeler::print(std::begin(heap.Data()), std::end(heap.Data()));
+    //Keeler::print(std::begin(heap.Data()), std::end(heap.Data()));
 
     return 0;
 }
