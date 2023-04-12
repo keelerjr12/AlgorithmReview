5bc55b10b2a6dc83f7aa8e0dcc631fee1237dad1
Josh Keeler <keelerjr12@gmail.com>
2023-04-11 21:16:57 -0500

    working on hashmap

--- chp11.h
+++ chp11.h
@@ -1,10 +1,35 @@
+#include <cstddef>
+#include <functional>
+
 namespace keeler {
 
-  template<typename T>
+  template<typename Key, typename T, typename Hash = std::hash<Key> >
   class UnorderedMap {
-
-    void test() {
+   public:
+    
+    bool empty() const {
+      return size() == 0;
     }
 
+    std::size_t size() const {
+      return m_size;
+    }
+
+    void insert(const T& val) {
+      if (empty()) {
+        m_data = new T[10];
+      }
+
+      const auto hash = hasher(val);
+      m_data[hash] = val;
+
+      ++m_size;
+    }
+
+   private:
+
+    std::size_t m_size = 0;
+    T* m_data = nullptr;
+    Hash hasher;
   };
 }
--- unordered_map_tests.cpp
+++ unordered_map_tests.cpp
@@ -2,7 +2,18 @@
 #include "chp11.h"
 
 TEST(UnorderedMap, map_empty_returns_empty_true) {
-  const auto a = true;
+  keeler::UnorderedMap<int, int> map;
 
-  EXPECT_TRUE(a);
+  const auto is_empty = map.empty();
+
+  EXPECT_TRUE(is_empty);
+}
+
+TEST(UnorderedMap, map_empty_insert_single_empty__empty_false) {
+  keeler::UnorderedMap<int, int> map;
+
+  map.insert(3);
+  const auto is_empty = map.empty();
+
+  EXPECT_FALSE(is_empty);
 }
