00812400f9ec75974c1cefd529739b19c5036fde
Josh Keeler <keelerjr12@gmail.com>
2023-04-22 13:05:39 -0500

    HT insert working correctly -- still need to refactor

--- chp11.h
+++ chp11.h
@@ -50,7 +50,6 @@ namespace keeler {
 
    public:
     UnorderedMapIterator(detail::HashNode<T>* node) {
-      std::cout << node << std::endl;
       m_node = node;
     }
 
@@ -59,11 +58,15 @@ namespace keeler {
       return UnorderedMapIterator<T>(m_node);
     }
 
+    T& operator*() {
+      return m_node->val;
+    }
+
     inline bool operator==(const UnorderedMapIterator<T>& rhs) {
       return m_node == rhs.m_node;
     }
 
-    bool operator!=(const UnorderedMapIterator<T>& rhs) {
+    inline bool operator!=(const UnorderedMapIterator<T>& rhs) {
       return !(*this == rhs);
     }
 
@@ -89,46 +92,42 @@ namespace keeler {
 
     bool insert(const T& val) {
       if (empty()) {
-
         m_bkts = new detail::HashNode<T>*[10] { nullptr };
         m_bkt_ct = 10;
+      }
 
-        const auto hash = hasher(val);
-        m_bkts[hash % m_bkt_ct] = &m_before_begin;
+      const auto hash = hasher(val);
+      auto bkt_ptr = &m_bkts[hash % m_bkt_ct];
 
-        auto new_node = new detail::HashNode<T>();
-        new_node->val = val;
-        m_before_begin.m_next = new_node;
+      if (*bkt_ptr == nullptr) {
+        auto new_begin = new detail::HashNode<T>();
+        new_begin->val = val;
+        new_begin->m_next = m_before_begin.m_next;
 
-        ++m_el_ct;
+        if (m_before_begin.m_next) {
+          const auto prev_begin_bkt_ptr = &m_bkts[hasher(m_before_begin.m_next->val) % m_bkt_ct];
+          *prev_begin_bkt_ptr = new_begin;
+        }
+
+        m_before_begin.m_next = new_begin;
+        *bkt_ptr = &m_before_begin;
 
       } else {
-
-        const auto hash = hasher(val);
-        const auto bkt = m_bkts[hash % m_bkt_ct];
-
-        if (bkt == nullptr) {
-          auto new_begin = new detail::HashNode<T>();
-          new_begin->val = val;
-          new_begin->m_next = &m_before_begin;
-          m_before_begin = *new_begin;
-          m_bkts[hash % m_bkt_ct] = &m_before_begin;
-        } else {
-          auto node = bkt->m_next;
-          while (node && (hasher(node->val) == hash)) {
-            if (node->val == val) {
-              return true;
-            }
-            node = node->m_next;
+        auto node = (*bkt_ptr)->m_next;
+        while (node && (hasher(node->val) == hash)) {
+          if (node->val == val) {
+            return true;
           }
-          auto new_begin = new detail::HashNode<T>();
-          new_begin->val = val;
-          new_begin->m_next = bkt;
-          m_bkts[hash % m_bkt_ct] = new_begin;
+          node = node->m_next;
         }
-          
-        ++m_el_ct;
+
+        auto new_begin = new detail::HashNode<T>();
+        new_begin->val = val;
+        new_begin->m_next = (*bkt_ptr)->m_next;
+        (*bkt_ptr)->m_next = new_begin;
       }
+      
+      ++m_el_ct;
 
       return false;
     }
--- main.cpp
+++ main.cpp
@@ -4,11 +4,22 @@
 int main() {
   keeler::UnorderedMap<int, int> map;
   map.insert(1);
+  map.insert(11);
+  map.insert(3);
+  map.insert(2);
+  map.insert(21);
+  map.insert(45);
+  map.insert(13);
+  map.insert(35);
+  map.insert(22);
+  map.insert(52);
 
   for (auto it = map.begin(); it != map.end(); ++it)
   {
-    std::cout << "test" << std::endl;
+    std::cout << *it << " ";
   }
+
+  std::cout << "\n";
 
   return 0;
 }
