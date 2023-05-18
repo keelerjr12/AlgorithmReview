3a1be4d6bdf865a3c664b474fab6ade648b4fb63
Josh Keeler <keelerjr12@gmail.com>
2023-05-17 21:32:02 -0500

    working RB tree insert

--- main.cpp
+++ main.cpp
@@ -2,17 +2,13 @@
 #include <string>
 #include "chp13.h"
 
-<<<<<<< HEAD
 template<typename Key, typename T>
 void preorder_print(keeler::RbTree<Key, T>& tree);
 
-=======
->>>>>>> 5438969caa2f114f83f29f86c13bcfe670100506
 int main() {
   keeler::RbTree<int, int> tree;
 
   tree.insert({3, 3});
-<<<<<<< HEAD
   preorder_print(tree);
   tree.insert({1, 1});
   preorder_print(tree);
@@ -28,15 +24,6 @@ int main() {
   preorder_print(tree);
   tree.insert({7, 7});
   preorder_print(tree);
-=======
-  tree.insert({1, 1});
-  tree.insert({5, 5});
-  tree.insert({0, 0});
-  tree.insert({6, 6});
-  tree.insert({2, 2});
-  tree.insert({4, 4});
-  tree.insert({7, 7});
->>>>>>> 5438969caa2f114f83f29f86c13bcfe670100506
   
   std::cout << tree.size() << std::endl;
 
@@ -46,15 +33,12 @@ int main() {
 
   std::cout << "\n";
 
-<<<<<<< HEAD
 
   return 0;
 }
 
 template<typename Key, typename T>
 void preorder_print(keeler::RbTree<Key, T>& tree) {
-=======
->>>>>>> 5438969caa2f114f83f29f86c13bcfe670100506
   for (auto it = tree.preorder_begin(); it != tree.preorder_end(); ++it) {
     std::string spacing(2*it.depth(), ' ');
     std::cout << spacing << "(" << it->first << ", " << it->second << ")";
@@ -65,10 +49,5 @@ void preorder_print(keeler::RbTree<Key, T>& tree) {
     }
   }
 
-<<<<<<< HEAD
   std::cout << "\n";
-=======
-
-  return 0;
->>>>>>> 5438969caa2f114f83f29f86c13bcfe670100506
 }
